4fbc6b6605bb819e1064d4ba35f8e4a3eca4ee72
Kevin Origho <orighokevin@gmail.com>
2022-06-30 13:59:49 +0100

    polls adjustment: renaming the question and choices

--- polls/models.py
+++ polls/models.py
@@ -18,6 +18,6 @@ class Choices(models.Model):
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.choice_answer
+        return f"{self.question} ---> {self.choice_answer}"
 
 
